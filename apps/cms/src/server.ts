712a2a24ae6ee179723ce381fddba36d22dbb681
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-02-27 20:21:57 -0500

    add payload cms in apps

---
+++ apps/cms/src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import payload from "payload";
+const path = require("path");
+
+require("dotenv").config({
+  path: path.resolve(__dirname, "../../../.env.local"),
+});
+const app = express();
+
+// Redirect root to Admin panel
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
+
+const start = async () => {
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    mongoURL: process.env.MONGODB_URI,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+    },
+  });
+
+  // Add your own express routes here
+
+  app.listen(8080);
+};
+
+start();
